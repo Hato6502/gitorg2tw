da3c0a77fae849bf6f9cac9afc4107c765304ac4
hato6502 <erisuke12345@yahoo.co.jp>
2019-02-14 23:43:09 +0900

    Added signature.

--- html/index.php
+++ html/index.php
@@ -1,3 +1,10 @@
 <?php
-    error_log(print_r($_POST, true));
+    require '/var/www/config.php';
+
+    $header = getallheaders();
+    $hmac = hash_hmac($HTTP_RAW_POST_DATA, SECRET);
+    if (isset($header['X-Hub-Signature']) && $header['X-Hub-Signature']==='sha1='.$hmac) {
+        error_log('OK');
+    }
+    // error_log(print_r($_POST, true));
 ?>
