d5e355361e5475043585ed8da4663df04db0d64a
hato6502 <erisuke12345@yahoo.co.jp>
2019-02-14 23:54:48 +0900

    Fixed hmac function.

--- html/index.php
+++ html/index.php
@@ -2,7 +2,7 @@
     require '/var/gitorg2tw/config.php';
 
     $header = getallheaders();
-    $hmac = hash_hmac($HTTP_RAW_POST_DATA, SECRET);
+    $hmac = hash_hmac('sha1', $HTTP_RAW_POST_DATA, SECRET);
     if (isset($header['X-Hub-Signature']) && $header['X-Hub-Signature']==='sha1='.$hmac) {
         error_log('OK');
     }
