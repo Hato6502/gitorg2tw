9f717b1844cbc108b80c1abe92451f5a622b48de
hato6502 <erisuke12345@yahoo.co.jp>
2019-02-14 23:57:38 +0900

    Fixed HTTP_RAW_POST_DATA.

--- html/index.php
+++ html/index.php
@@ -2,9 +2,11 @@
     require '/var/gitorg2tw/config.php';
 
     $header = getallheaders();
-    $hmac = hash_hmac('sha1', $HTTP_RAW_POST_DATA, SECRET);
+    $hmac = hash_hmac('sha1', file_get_contents("php://input"), SECRET);
     if (isset($header['X-Hub-Signature']) && $header['X-Hub-Signature']==='sha1='.$hmac) {
         error_log('OK');
+    }else{
+        error_log('NG');
     }
     // error_log(print_r($_POST, true));
 ?>
